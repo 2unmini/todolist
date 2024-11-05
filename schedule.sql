2e54d38626609d3457380fc8e0f92855d6f60dda
2unmini <2unmin4030@gmail.com>
2024-11-05 15:25:49 +0900

    Update schedule.sql
    SQL문 수정

--- schedule.sql
+++ schedule.sql
@@ -1,39 +1,29 @@
-user 테이블 생성
-CREATE TABLE user (
-  `user_id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (`user_id`));
-
 schedule 테이블 생성
 
-CREATE TABLE schedule (
-  `schedule_id` INT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(45) NOT NULL,
-  `writer_id` INT NOT NULL,
-  `content` VARCHAR(100) NULL,
-  `reg_date` DATE NOT NULL,
-  `mod_date` DATE NULL,
-  `password` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`schedule_id`),
-  INDEX `fk_schedule_user_idx` (`writer_id` ASC) ,
-  CONSTRAINT `fk_schedule_user`
-    FOREIGN KEY (`writer_id`)
-    REFERENCES user (`user_id`);
+CREATE TABLE schedule
+(
+    schedule_id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(45) NOT NULL ,
+    username VARCHAR(50) NOT NULL ,
+    content VARCHAR(100) NULL ,
+    password VARCHAR(20) NOT NULL ,
+    created_at DATE NOT NULL ,
+    updated_at DATE NULL,
+    PRIMARY KEY (schedule_id)
+)
 
 일정 추가
-insert into schedule (title,content,password)
-values("제목,내용","비밀번호");
+insert into schedule (title,userName,content,password)
+values("제목,"작성자명",내용","비밀번호");
 
 전체 일정 조회
-select schedule.*,user.name AS writer
-from schedule
-join user on schedule.writer_id=user.user_id;
+select *
+from schedule;
 
 상세 일정 조회
-select schedule.*,user.name AS writer
+select *
 from schedule
-join user on schedule.writer_id=user.user_id;
-where schedule.schedule_id = 고유번호;
+where schedule_id = 고유번호;
 
 선택 일정의 제목 변경 
 update schedule set title = 입력값
